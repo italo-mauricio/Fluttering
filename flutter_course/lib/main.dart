bafd7b2a1ca27d75132150363e41cf1654b740fe
italo <italogithub@gmail.com>
2023-04-03 10:56:33 -0300

    feat: modularization

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -1,47 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'app_widget.dart';
+
 void main() {
   runApp(AppWidget(
     title: "Fluttering",
   ));
 }
 
-class AppWidget extends StatelessWidget {
-  final String title;
 
-  const AppWidget({super.key, required this.title});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.amber),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() {
-    return HomePageState();
-  }
-}
-
-class HomePageState extends State<HomePage> {
-  int count = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-          child: GestureDetector(
-        child: Text('Contador: $count'),
-        onTap: () {
-          setState(() {
-            count++;
-          });
-        },
-      )),
-    );
-  }
-}
--- flutter_course/test/widget_test.dart
+++ flutter_course/test/widget_test.dart
@@ -6,6 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_course/app_widget.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_course/main.dart';
