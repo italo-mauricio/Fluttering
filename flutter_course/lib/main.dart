0d5f68a0f67c1c3924f480fed75f3ae84b6d4188
italo <italogithub@gmail.com>
2023-04-13 12:46:38 -0300

    criando appwidget do zero

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -11,3 +11,18 @@ main() {
     ),
   ));
 }
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(
+        child: Text(
+          'Aplicativo Inicial',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.black, fontSize: 20.0),
+        ),
+      ),
+    );
+  }
+}
