04f5b0bee841ca9669f4d43fd4ab0091832d9924
italo <italogithub@gmail.com>
2023-05-28 02:01:05 -0300

    update

--- conversor_moedas/lib/app/Views/home_page.dart
+++ conversor_moedas/lib/app/Views/home_page.dart
@@ -6,10 +6,30 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Image.asset('assets/logo.png'),
-        ],
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            Image.asset('assets/logo.png'),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                children: [
+                  Expanded(child: DropdownButton(items: [], onChanged: (value) {})),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => {}, 
+              child: Text('Converter'))
+          ],
+        ),
       ),
     );
   }
