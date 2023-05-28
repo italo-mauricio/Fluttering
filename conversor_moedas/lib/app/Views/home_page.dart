388a2800ec66aa40c9abc44675c7a9db6a97a0d0
italo <italogithub@gmail.com>
2023-05-28 02:08:25 -0300

    add padding

--- conversor_moedas/lib/app/Views/home_page.dart
+++ conversor_moedas/lib/app/Views/home_page.dart
@@ -9,26 +9,28 @@ class HomePage extends StatelessWidget {
       body: SizedBox(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: Column(
-          children: [
-            Image.asset('assets/logo.png'),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Row(
-                children: [
-                  Expanded(child: DropdownButton(items: [], onChanged: (value) {})),
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20, top: 100, bottom: 20),
+          child: Column(
+            children: [
+              Image.asset('assets/logo.png', width: 150, height: 150),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: DropdownButton(items: [], onChanged: (value) {})),
+                    const Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: () => {}, 
-              child: Text('Converter'))
-          ],
+              ElevatedButton(onPressed: () => {}, child: Text('Converter'))
+            ],
+          ),
         ),
       ),
     );
